1eab9bed33a4735a2cf16c61e86a207f3f499f80
nomad <nomad@debian.nomaddebian.net>
2024-08-12 22:16:21 +0200

    Trade Producer Script Working

---
+++ MicroServices/TradeProducer/source/FetchTrades.py
@@ -0,0 +1,83 @@
+from websocket import create_connection
+
+from typing import List, Dict
+import json
+from pprint import pp
+
+class KrakenWebSocket:
+
+    def __init__(self, product_id:str, URL:str, channel:str):
+        self.ProductID = product_id
+        self.URL = URL
+        self.Channel = channel
+
+        self._ws = create_connection(self.URL)
+
+        self.Subscibe()
+
+    def Subscibe(self):
+        '''
+        Dummy Function that Subscribes to the Kraken API WebSocket
+        '''
+        
+        SubscribingMessage = \
+        {
+            "method": "subscribe",
+            "params": {
+                "channel": self.Channel,
+                "symbol": [
+                    self.ProductID
+                ],
+                "snapshot": False
+            }
+        }
+
+        try:
+            self._ws.send(json.dumps(SubscribingMessage))
+
+        except Exception as e:
+            # Handle exception
+            print(f"An error occurred whilist trying to Subscribe to the KrakenAPI WebSocket: {e}")
+        
+        else:
+            print("Successfully Subscribed to the KrakenAPI WebSocket.")
+            #Then, Dumping the First 2 Useless Messages. Connection ID and Connection Status
+            _ = self._ws.recv()
+            _ = self._ws.recv()
+
+
+    def FetchTrades(self) -> List[Dict]:
+        
+        Message = self._ws.recv()
+
+        print(f'Message Received: {Message}')
+
+        #Data is returned in the str format like as follows:
+        #'{"channel":"trade","type":"update","data":[{"symbol":"BTC/USD","side":"sell","price":59121.8,"qty":0.00022946,"ord_type":"market","trade_id":72823289,"timestamp":"2024-08-12T19:38:30.575191Z"}]}''
+        
+        #Now, checking if the there is any data in the message or if it contains the heartbeat (no Data Update)
+        if "heartbeat" in Message:
+            #Rerutning an Empty List
+            return []
+
+        else:
+            Message = json.loads(Message)
+            Trades = []
+            
+            for trade in Message["data"]:
+                #Extract Trade info, there could be more than one message at time, building smaller and smarter dict.
+                Trades.append({
+                                "product_id": self.ProductID,
+                                "price": trade["price"],
+                                "volume": trade["qty"],
+                                "timestamp": trade["timestamp"]
+                              }
+                             )
+            
+            #pp(Message)
+
+        #Interactive command to actually debug code from a snapshot
+        #breakpoint()
+
+        return Trades
+    
---
+++ MicroServices/TradeProducer/source/ProducerQuixStreamsApp.py
@@ -0,0 +1,66 @@
+# Create an Application instance with Kafka configs
+from quixstreams import Application
+
+from FetchTrades import KrakenWebSocket
+
+from typing import List, Dict
+from time import sleep
+
+def ProduceTrades(
+    kafka_broker_address: str,
+    kafka_input_topic: str
+) -> None:
+    
+    '''
+    Reads Trades from Kraken WebSocket API and Pushes them through the TradeProducer, saving them into a Kafka Topic.
+
+    Args:
+        kafka_broker_address (str): The address of the Kafka Broker (Containerized with RedPanda)
+        kafka_input_topic (str): The name of the Kafka Input Topic
+
+    Returns: 
+        None
+    '''
+
+    # Defining the Application
+    app = Application(
+        broker_address = kafka_broker_address, consumer_group = "TradeProducer"
+    )
+
+    #Defining the topic with JSON serialization where we'll save the Trades
+    topic = app.topic(name = kafka_input_topic, value_serializer = "json")
+
+    #Fake Test Event
+    #event = {"id": "1", "text": "Lorem ipsum dolor sit amet"}
+    
+    productid = "BTC/USD"
+    URL = "wss://ws.kraken.com/v2"
+    channel = "trade"
+
+    #Establishing Kraken WebSocket Connection
+    KrakenAPI = KrakenWebSocket(product_id = "BTC/USD", URL = URL, channel = channel)
+    #KrakenAPI.Subscibe() <--- Added to init Method of Class
+
+    #Creating a Producer instance
+    with app.get_producer() as producer:
+
+        while True:
+            #getting Trades
+            Trades: List[Dict] = KrakenAPI.FetchTrades()
+            
+            for T in Trades:
+
+                #Serializing the event using the defined Topic 
+                message = topic.serialize(key = T["product_id"], 
+                                          value = T)
+               
+                #Producing a message into the Kafka topic
+                producer.produce(topic = topic.name, 
+                            value = message.value, 
+                            key = message.key)
+
+if __name__ == "__main__":
+    ProduceTrades(
+        kafka_broker_address = "localhost:19092",
+        kafka_input_topic = "Trades"
+    )
